7afbb14112df28f9730926f4cda70e05370cb049
Shou Ya <zxyzxy12321@gmail.com>
2013-01-08 13:07:08 +0800

    basic lexical scanner

---
+++ lib/scanner_rules.rb
@@ -0,0 +1,29 @@
+
+
+module Revo
+  class Scanner
+    rule(/\s+/) { :PASS }
+    rule(/\r\n|\r|\n/) { increase_line_number; :PASS }
+
+    symbol_rule('(')  { [:LBRACKET, nil]  }
+    symbol_rule(')')  { [:RBRACKET, nil]  }
+    symbol_rule(',@') { [:COMMA_AT, nil]  }
+    symbol_rule(',')  { [:COMMA, nil]     }
+    symbol_rule("'")  { [:QUOTE, nil]     }
+    symbol_rule('&')  { [:AMPERSAND, nil] }
+    symbol_rule('`')  { [:BACKQUOTE, nil] }
+
+    rule(/\d+\.\d*/) { [:FLOAT, parse_float(@match)] }
+    rule(/\d+/)      { [:INTEGER, parse_int(@match)] }
+
+    symbol_rule('"')         { @state = :DSTR; @buffer[:str] = ''; :PASS }
+    symbol_rule('\"', :DSTR) { @buffer[:str] << '"'; :PASS }
+    symbol_rule('"', :DSTR)  { @state = nil; [:STRING, @buffer.delete(:str)] }
+#    symbol_rule('\0[xX]\d+') {  }
+    rule(/./, :DSTR)         { @buffer[:str] << @match; :PASS }
+
+    rule(/[\w_\-\@\?\$\%\^\*\+\/\~]+/)   { [:NAME, @match] }
+    rule(/\:[\w_\-\@\?\$\%\^\*\+\/\~]+/) { [:SYMBOL, @match] }
+
+  end
+end
